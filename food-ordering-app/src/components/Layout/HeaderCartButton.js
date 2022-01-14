7760c094366f2d4394e03e9f0626ac3a41759f9c
Bas Bargagli <bas.bargagli@gmail.com>
2022-01-13 23:32:35 -0600

    cart button popout

--- food-ordering-app/src/components/Layout/HeaderCartButton.js
+++ food-ordering-app/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import styles from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfItemsInCart = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0)
 
+  const buttonStyles = `${styles.button} ${buttonIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if(cartCtx.items.length === 0){
+      return;
+    }
+    setButtonIsHighlighted(true);
+
+    const timer = setTimeout(() => {setButtonIsHighlighted(false)}, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={styles.button} onClick={props.openCart}>
+    <button className={buttonStyles} onClick={props.openCart}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
