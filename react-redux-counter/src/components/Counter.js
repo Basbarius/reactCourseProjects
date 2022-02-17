10b7ce957e4f092c1189bbecbc50ff7c60b03e58
Bas Bargagli <bas.bargagli@gmail.com>
2022-02-17 13:52:16 -0600

    implemented redux toolkit to simplify actions and reducers

--- react-redux-counter/src/components/Counter.js
+++ react-redux-counter/src/components/Counter.js
@@ -3,33 +3,41 @@
 import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
+import { counterActions } from '../store/index';
 
 const Counter = () => {
   //here a function is passed which will tell what part of the store wants to be accessed to
   //this hook also subscribes the component to the store, so if the state changes, this will rerender
   //this is the hook equivalent of mapStateToProps
-  const counter = useSelector(state => state.counter);
-  const showCounter = useSelector(state => state.showCounter);
+  //make sure to reference the slice name declared on the configureStore, then access the state piece
+  const counter = useSelector(state => state.counter.counter);
+  const showCounter = useSelector(state => state.counter.showCounter);
   //useDispatch is called without arguments and will return a dispatch function
   //this is the hook equivalent of mapDispatchToProps
   const dispatch = useDispatch();
 
   //actions can be passed through the dispatch function, just make sure to use the same action type
   const incrementHandler = () => {
-    dispatch({ type: 'INCREASE'});
+    //dispatch({ type: 'INCREASE'});
+    //redux toolkit actions are methods, thus they should be passed as such to the dispatch
+    dispatch(counterActions.increment())
   };
 
   //a payload can be added to provide data to the reducer in order to perform a state change
   const incrementMultiHandler = () => {
-    dispatch({ type: 'INCREASE_MULTI', payload: 5});
+    //dispatch({ type: 'INCREASE_MULTI', payload: 5});
+    //when passing in payloads, redux toolkit will pass the values as payload automatically
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'DECREASE'});
+    //dispatch({ type: 'DECREASE'});
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'TOGGLE'});
+    //dispatch({ type: 'TOGGLE'});
+    dispatch(counterActions.toggle());
   };
 
   return (
--- react-redux-counter/src/store/index.js
+++ react-redux-counter/src/store/index.js
@@ -1,41 +1,79 @@
 //import in react style
-import { createStore } from 'redux';
+//import { createStore } from 'redux';
+
+//more powerful version of createReducer, creates a slice in redux state
+//configureStore replaces combine reducers in order to combine slices in a store
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0, 
   showCounter: true
 };
 
-//reducer with same logic as redux-demo
-const counterReducer = (state = initialState, action) => {
-  if(action.type === "INCREASE"){
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
+//a redux state slice has a name and initial state, which returns a piece of state
+//a slice also needs reducers
+/*instead of writing if or case statement in a reducer, several reducers are declared with 
+their own logic in order to reduce boiler plate code*/
+//slice reducers can mutate state since the library will correct it behind the scenes
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
     }
   }
-  if(action.type === "INCREASE_MULTI"){
-    return {
-      counter: state.counter + action.payload,
-      showCounter: state.showCounter
-    }
-  }
-  if(action.type === "DECREASE"){
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    }
-  }
-  if(action.type === 'TOGGLE'){
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter
-    }
-  }
-  return state;
-}
+});
 
-const store = createStore(counterReducer);
+//reducer with same logic as redux-demo
+//reducers should never mutate the current state, but return a new one
+// const counterReducer = (state = initialState, action) => {
+//   if(action.type === "INCREASE"){
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter
+//     }
+//   }
+//   if(action.type === "INCREASE_MULTI"){
+//     return {
+//       counter: state.counter + action.payload,
+//       showCounter: state.showCounter
+//     }
+//   }
+//   if(action.type === "DECREASE"){
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter
+//     }
+//   }
+//   if(action.type === 'TOGGLE'){
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter
+//     }
+//   }
+//   return state;
+// }
+ 
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer
+  }
+});
+//to connect slices to store, a reducer object is created that contains a map of all reducers
+
+//actions can be created automatically by redux toolkit using the actions property
+//actions are methods in this case
+export const counterActions = counterSlice.actions;
 
 //make the store available for other components in the app
 export default store;
