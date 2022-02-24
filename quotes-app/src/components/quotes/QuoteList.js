5465facb381dd0fb009bd33a245baa540ca2723e
Bas Bargagli <bas.bargagli@gmail.com>
2022-02-24 00:51:01 -0600

    useLocation, search params and more nested routing

--- quotes-app/src/components/quotes/QuoteList.js
+++ quotes-app/src/components/quotes/QuoteList.js
@@ -1,13 +1,42 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  //an extended version of useParams is useLocation, a hook that can exctract the search parameters
+  const history = useHistory();
+  const location = useLocation();
+
+  //URLSearchParams is a built in JS class that will process the parameters given by location
+  const queryParams = new URLSearchParams(location.search);
+  //using get method, search parameters can be obtained by key
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort {isSortingAscending ? 'Descending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- quotes-app/src/pages/QuoteDetail.js
+++ quotes-app/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams, Route } from 'react-router-dom'
+import { useParams, Route, Link } from 'react-router-dom'
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments';
@@ -18,9 +18,15 @@ const QuoteDetail = () => {
     return <p>No quote found</p>
   }
   
+  //use links to travel to nested routes and play with them
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author}/>
+      <Route exact path={`/quotes/${params.quoteId}`}>
+        <div className="centered">
+          <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+        </div>
+      </Route>
       <Route exact path={`/quotes/${params.quoteId}/comments`} component={Comments} />
     </>
   )
